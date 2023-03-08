7deea8351a5fb467219ea4bbe0ad9d7780e3ab6f
Amlan Talukder <amlanaccount@gmail.com>
2023-03-08 21:09:24 +0000

    test for cd

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def home():
 
 @app.route("/<msg>", methods=["GET"])
 def welcome(msg):
-    return f"<h>Hello World {msg}!!!</h>"
+    return f"<h>Hello World !!! {msg}!!!</h>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
