914f4121e5820328849b58b31fe8f7ac1793e2e6
Amlan Talukder <amlanaccount@gmail.com>
2023-03-10 14:45:23 +0000

    Changed port

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def home():
 
 @app.route("/<msg>", methods=["GET"])
 def welcome(msg):
-    return f"<h>Hello World !!! {msg}!!!</h>"
+    return f"<h>Hello World !! {msg}!!!</h>"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="127.0.0.1", port=8080)
